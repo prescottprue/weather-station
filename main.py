9018a672819a9ac7a0fbed5ed0e89daa1721df02
Scott Prue <scott@sideinc.com>
2024-01-01 01:38:03 -0700

    fix: add git reset to main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return {"status": "active", "version": "1.0.0"}
+  return {"status": "active", "version": "1.0.1"}
 
 # TODO: Add auth header
 @app.get("/measurements")
